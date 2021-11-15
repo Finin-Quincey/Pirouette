b92a53dc1f9446b77e1910672b2db4ab56753864
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2021-11-15 19:47:49 +0000

    Very rudimentary code to display digits 0-9 in sequence

--- src/test/test_rig_controller/test_rig_controller.ino
+++ src/test/test_rig_controller/test_rig_controller.ino
@@ -3,8 +3,8 @@
 
 const int STEPS_PER_REVOLUTION = 2048;
 
-Stepper upperStepper(STEPS_PER_REVOLUTION, 8, 9, 10, 11);
-Stepper lowerStepper(STEPS_PER_REVOLUTION, 2, 3, 4, 5);
+Stepper upperStepper(STEPS_PER_REVOLUTION, 8, 10, 9, 11);
+Stepper lowerStepper(STEPS_PER_REVOLUTION, 2, 4, 3, 5);
 
 const int SWITCH_PIN = 12;
 
@@ -13,13 +13,18 @@ const int LOWER_STEPPER_OFFSET = 700;
 
 const double DEGREES_PER_STEP = STEPS_PER_REVOLUTION / 360.0;
 
+const double UPPER_ANGLES[] = {  0,  97,  70,  61, 231, 283, 283,   0, 180, 283};
+const double LOWER_ANGLES[] = {  0, 180,  72, 270, 180, 270,   0, 180,  12, 180};
+
+int displayedNumeral = 0;
+
 void setup(){
 
     // Pin configuration
     pinMode(SWITCH_PIN, INPUT_PULLUP);
 
-    upperStepper.setSpeed(50);
-    lowerStepper.setSpeed(50);
+    upperStepper.setSpeed(6);
+    lowerStepper.setSpeed(6);
 
     Serial.begin(9600); // Start the serial monitor
 
@@ -31,13 +36,21 @@ void setup(){
     // Zero both motors
     zero(upperStepper);
     zero(lowerStepper);
-    // Apply offsets to get to zero
+    // Apply offsets to display the numeral zero
     upperStepper.step(UPPER_STEPPER_OFFSET);
     lowerStepper.step(LOWER_STEPPER_OFFSET);
 }
 
 void loop(){
 
+    delay(3000);
+
+    int prevDisplayedNumeral = displayedNumeral;
+    displayedNumeral = (displayedNumeral + 1) % 10;
+
+    // Really crude test for the time being, just move by the difference between successive numerals, clamped at -180 to 180
+    upperStepper.step(angleToSteps(int(UPPER_ANGLES[displayedNumeral] - UPPER_ANGLES[prevDisplayedNumeral] + 180) % 360 - 180));
+    lowerStepper.step(angleToSteps(int(LOWER_ANGLES[displayedNumeral] - LOWER_ANGLES[prevDisplayedNumeral] + 180) % 360 - 180));
 }
 
 // Helper Methods
