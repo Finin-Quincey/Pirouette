7e6ebe0c1e9e1479e3ec430636ea191a96365e22
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2022-04-03 19:19:23 +0100

    Implement ability to set the time

--- src/main/src/state/SetHoursState.cpp
+++ src/main/src/state/SetHoursState.cpp
@@ -9,5 +9,20 @@ void SetHoursState::onEntry(){
 }
 
 State* SetHoursState::update(){
+
+    if(selectBtn.getHoldTime() > SELECT_HOLD_TIME) return STATE_CLOCK; // Finished setting clock
+
+    if(selectBtn.justPressed()) return STATE_SET_MINS;
+
+    // Bit of a weird way of doing it but it avoids duplicating code without having to declare a separate function
+    int delta = plusBtn.justPressed() - minusBtn.justPressed(); // Will be either -1, 0 or 1
+
+    if(delta != 0){
+        DateTime now = rtc.now();
+        uint8_t hours = modulo(now.hour() + delta, 24);
+        rtc.adjust(DateTime(now.year(), now.month(), now.day(), hours, now.minute(), now.second()));
+        setHours(hours, false);
+    }
+
     return this;
 }
--- src/main/src/state/SetMinsState.cpp
+++ src/main/src/state/SetMinsState.cpp
@@ -9,5 +9,20 @@ void SetMinsState::onEntry(){
 }
 
 State* SetMinsState::update(){
+
+    if(selectBtn.getHoldTime() > SELECT_HOLD_TIME) return STATE_CLOCK; // Finished setting clock
+
+    if(selectBtn.justPressed()) return STATE_SET_HOURS;
+
+    // Bit of a weird way of doing it but it avoids duplicating code without having to declare a separate function
+    int delta = plusBtn.justPressed() - minusBtn.justPressed(); // Will be either -1, 0 or 1
+
+    if(delta != 0){
+        DateTime now = rtc.now();
+        uint8_t minutes = modulo(now.minute() + delta, 60);
+        rtc.adjust(DateTime(now.year(), now.month(), now.day(), now.hour(), minutes, now.second()));
+        setMinutes(minutes, false);
+    }
+
     return this;
 }
